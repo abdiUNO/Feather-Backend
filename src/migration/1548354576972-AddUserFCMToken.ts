d15a8b57146c96aec4fbba7b7242a0540dfe1877
abdullahi <abdullahimahamed0987@gmail.com>
2019-01-24 12:29:50 -0600

    removed migrations

--- src/migration/1548354576972-AddUserFCMToken.ts
+++ src/migration/1548354576972-AddUserFCMToken.ts
@@ -1,12 +1,12 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AddUserFCMToken1548351464308 implements MigrationInterface {
+export class AddUserFCMToken1548354576972 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.addColumn(
       'user',
       new TableColumn({
         name: 'fcmToken',
-        type: 'varchar(255)',
+        type: 'varchar',
         isNullable: true,
       }),
     );
