e0f893b01062d514bf69f13832979ca5ddbf449e
abdullahi <abdullahimahamed0987@gmail.com>
2019-01-24 12:04:35 -0600

    added column links

---
+++ src/migration/1548353058993-PostRefactoring.ts
@@ -0,0 +1,47 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class PostRefactoring1548353058993 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.addColumn(
+      'post',
+      new TableColumn({
+        name: 'links',
+        type: 'int',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'post',
+      new TableForeignKey({
+        columnNames: ['links'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'microlink',
+      }),
+    );
+
+    await queryRunner.addColumn(
+      'post',
+      new TableColumn({
+        name: 'image',
+        type: 'longtext',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const table = await queryRunner.getTable('links');
+    const foreignKey = table.foreignKeys.find(
+      fk => fk.columnNames.indexOf('links') !== -1,
+    );
+    await queryRunner.dropForeignKey('post', foreignKey);
+    await queryRunner.dropColumn('post', 'links');
+    await queryRunner.dropColumn('post', 'image');
+  }
+}
